e0ce3a34b4a8ee77ebf372ff0c4c12dcad9e387e
priteshdev8767 <priteshpatil87679@gmail.com>
2026-03-21 12:18:27 +0530

    updatdsdss

--- lib/template.ts
+++ lib/template.ts
@@ -1,9 +1,9 @@
 // templatePaths.ts
 export const templatePaths = {
-  REACT: '/.vibecode-starters/react-ts',
-  NEXTJS: '/.vibecode-starters/nextjs',
-  EXPRESS: '/.vibecode-starters/express-simple',
-  VUE: '/.vibecode-starters/vue',
-  HONO: '/.vibecode-starters/hono-nodejs-starter',
-  ANGULAR: '/.vibecode-starters/angular',
+  REACT: '.vibecode-starters/react-ts',
+  NEXTJS: '.vibecode-starters/nextjs',
+  EXPRESS: '.vibecode-starters/express-simple',
+  VUE: '.vibecode-starters/vue',
+  HONO: '.vibecode-starters/hono-nodejs-starter',
+  ANGULAR: '.vibecode-starters/angular',
 };
